8ecaefe07b5bd9d737f1b953aba0c8c304d49eeb
Jelle Druyts <jelle.druyts@outlook.com>
2025-05-31 13:53:29 +0200

    Fix bootstrap process

--- bootstrap.bat
+++ bootstrap.bat
@@ -3,11 +3,11 @@ winget install -e --id Git.Git
 winget install -e --id Microsoft.PowerShell
 
 REM Clone the dotfiles repository
-mkdir "%USERPROFILE%\Code"
-"%PROGRAMFILES%\Git\cmd\git" clone https://github.com/jelledruyts/dotfiles.git "%USERPROFILE%\Code\dotfiles"
+mkdir "%USERPROFILE%\Local\Code"
+"%PROGRAMFILES%\Git\cmd\git" clone https://github.com/jelledruyts/dotfiles.git "%USERPROFILE%\Local\Code\dotfiles"
 
 @ECHO OFF
 ECHO.
 ECHO From a new ADMIN PowerShell Core terminal, you can now run the installation script:
-ECHO %USERPROFILE%\Code\dotfiles\install.ps1
+ECHO %USERPROFILE%\Local\Code\dotfiles\install.ps1
 ECHO.
